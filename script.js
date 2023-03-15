d8635657fc56ec0b24357becc25f48bb4fd1ed35
ETsBrother <etsbrother1@gmail.com>
2023-03-15 16:33:58 -0400

    added more changes

--- script.js
+++ script.js
@@ -208,3 +208,12 @@ function getCookie(cname) {
   }
   return "";
 }
+
+$(document).ready(function() {
+    $(window).keydown(function(event){
+      if(event.keyCode == 13) {
+        event.preventDefault();
+        return false;
+      }
+    });
+  });
